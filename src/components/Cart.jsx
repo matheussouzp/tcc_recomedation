a458081c4c317b999b063872f1b6fccaa52e77b7
William Czpak <wczpak@gmail.com>
2024-05-29 14:52:00 -0300

    commit cart com img

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import Footer from "./Footer";
 
 const Cart = () => {
+  const products = [
+    {
+      name: "Product Name 1",
+      price: 99.99,
+      imageUrl: "https://th.bing.com/th/id/OIP.kkfJZdixnZ7E13yXOvKTBAHaDf?w=294&h=180&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+    },
+    {
+      name: "Product Name 2",
+      price: 89.99,
+      imageUrl: "https://th.bing.com/th/id/OIP.-QQJODDzfyTP9vv_xqP1nAHaEc?pid=ImgDet&w=206&h=123&c=7&dpr=1,1"
+    },
+    {
+      name: "Product Name 3",
+      price: 79.99,
+      imageUrl: "https://th.bing.com/th/id/OIP.8uTpaoQQBOAeSWC0-nKqsAAAAA?w=300&h=237&rs=1&pid=ImgDetMain"
+    }
+  ];
+
   return (
     <div className="flex flex-col min-h-screen">
       <head>
@@ -46,22 +64,22 @@ const Cart = () => {
         </nav>
 
         {/* Main Content */}
-        <div className="container mx-auto mt-12 mb-16"> {/* Added margin bottom */}
+        <div className="container mx-auto mt-12 mb-16">
           <h2 className="text-3xl font-bold mb-8">Shopping Cart</h2>
           <div className="flex flex-col lg:flex-row lg:space-x-12">
             {/* Cart Items */}
             <div className="w-full lg:w-3/4 mb-8 lg:mb-0">
               <div className="bg-white p-6 rounded-lg shadow-lg">
-                {[...Array(3)].map((_, index) => (
+                {products.map((product, index) => (
                   <div key={index} className="flex items-center border-b border-gray-200 py-4">
                     <img
-                      src={`https://via.placeholder.com/100`}
+                      src={product.imageUrl}
                       alt={`Product Image ${index + 1}`}
                       className="w-20 h-20 object-cover rounded"
                     />
                     <div className="ml-4 flex-grow">
-                      <h3 className="text-xl font-bold">Product Name {index + 1}</h3>
-                      <p className="text-gray-600">${99.99 - index * 10}</p>
+                      <h3 className="text-xl font-bold">{product.name}</h3>
+                      <p className="text-gray-600">${product.price}</p>
                     </div>
                     <div className="flex items-center space-x-4">
                       <input
@@ -102,9 +120,10 @@ const Cart = () => {
           </div>
         </div>
 
-        {/* Footer */}
-        <Footer />
+
       </body>
+              {/* Footer */}
+              <Footer />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Cart from './components/Cart';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Index></Index>
+    <Cart></Cart>
   </React.StrictMode>
 );
 
